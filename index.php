649235b2e2c189962382bdbc94cb8f6583286493
basilmaben <basilmaben.bm@gmail.com>
2021-08-21 15:23:29 +0530

    uodate

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include_once("./views/layouts/main.handlebars"); ?>
+<?php include_once("./views/home.handlebars"); ?>
+<?php include_once("./views/login-signup.handlebars"); ?>
+<?php include_once("./views/submit.handlebars"); ?>
