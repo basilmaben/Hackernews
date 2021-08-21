9aa7fbfb6617110494b9eeaa735303f60d58d2c0
basilmaben <basilmaben.bm@gmail.com>
2021-08-21 15:21:55 +0530

    moved

--- views/index.php
+++ views/index.php
@@ -1,1 +1,4 @@
-<?php include_once("main.handlebars"); ?>
+<?php include_once("./layouts/main.handlebars"); ?>
+<?php include_once("home.handlebars"); ?>
+<?php include_once("login-signup.handlebars"); ?>
+<?php include_once("submit.handlebars"); ?>
